399913729d656022bd41edfc96ea5cc22bd7d344
pkkwilliam <pkkwilliam919@gmail.com>
2021-03-11 14:42:34 +0800

    add bitcode website contents

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -10,21 +10,30 @@ export class Contact extends Component {
               <div className="row">
                 <div className="section-title">
                   <h2>聯絡我們</h2>
-                  <p>
+                  {/* <p>
                     Please fill out the form below to send us an email and we
                     will get back to you as soon as possible.
-                  </p>
+                  </p> */}
                 </div>
-                <form name="sentMessage" id="contactForm" noValidate>
+                {/* <form action="https://mailthis.to/pkkwilliam919@gmail.com" 
+    method="POST"> 
+    <input type="text" name="name" placeholder="Your name" /> 
+    <input type="email" name="_replyto" placeholder="Your email" /> 
+    <input type="submit" value="Send" /> 
+</form>  */}
+<form action="https://mailthis.to/pkkwilliam919@gmail.com" 
+    method="POST"> 
                   <div className="row">
-                    <div className="col-md-6">
+                    <div className="col-md-6" >
                       <div className="form-group">
                         <input
                           type="text"
                           id="name"
                           className="form-control"
+                          name="name"
                           placeholder="Name"
                           required="required"
+                          style={{borderRadius: 5}}
                         />
                         <p className="help-block text-danger"></p>
                       </div>
@@ -35,8 +44,10 @@ export class Contact extends Component {
                           type="email"
                           id="email"
                           className="form-control"
+                          name="_replyto"
                           placeholder="Email"
                           required="required"
+                          style={{borderRadius: 5}}
                         />
                         <p className="help-block text-danger"></p>
                       </div>
@@ -50,22 +61,23 @@ export class Contact extends Component {
                       rows="4"
                       placeholder="Message"
                       required
-                    ></textarea>
+                      style={{borderRadius: 5}}
+                    />
                     <p className="help-block text-danger"></p>
                   </div>
                   <div id="success"></div>
-                  <button type="submit" className="btn btn-custom btn-lg">
-                    Send Message
+                  <button type="submit" className="btn btn-custom btn-lg" style={{width: '100%'}}>
+                    發送
                   </button>
                 </form>
               </div>
             </div>
             <div className="col-md-3 col-md-offset-1 contact-info">
               <div className="contact-item">
-                <h3>Contact Info</h3>
+                <h3>聯絡資訊</h3>
                 <p>
                   <span>
-                    <i className="fa fa-map-marker"></i> Address
+                    <i className="fa fa-map-marker"></i>地址
                   </span>
                   {this.props.data ? this.props.data.address : "loading"}
                 </p>
@@ -73,57 +85,61 @@ export class Contact extends Component {
               <div className="contact-item">
                 <p>
                   <span>
-                    <i className="fa fa-phone"></i> Phone
-                  </span>{" "}
+                    <i className="fa fa-phone"></i>電話
+                  </span>
                   {this.props.data ? this.props.data.phone : "loading"}
                 </p>
               </div>
-              <div className="contact-item">
+              <div className="contact-item" >
                 <p>
                   <span>
-                    <i className="fa fa-envelope-o"></i> Email
-                  </span>{" "}
+                    <i className="fa fa-envelope-o"></i>Email
+                  </span>
                   {this.props.data ? this.props.data.email : "loading"}
                 </p>
               </div>
             </div>
-            <div className="col-md-12">
-              <div className="row">
-                <div className="social">
-                  <ul>
-                    <li>
-                      <a
-                        href={this.props.data ? this.props.data.facebook : "/"}
-                      >
-                        <i className="fa fa-facebook"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href={this.props.data ? this.props.data.twitter : "/"}>
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href={this.props.data ? this.props.data.youtube : "/"}>
-                        <i className="fa fa-youtube"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+          {/* <SocialMedia {...this.props}/> */}
           </div>
         </div>
         <div id="footer">
           <div className="container text-center">
             <p>
-              &copy; 2021 __ Technology Ltd. Design By ME
+              &copy; 2021 Bitcode Technology LTD.
             </p>
           </div>
         </div>
       </div>
     );
   }
+}
+
+function SocialMedia(props) {
+  return             <div className="col-md-12">
+  <div className="row">
+    <div className="social">
+      <ul>
+        <li>
+          <a
+            href={props.data ? props.data.facebook : "/"}
+          >
+            <i className="fa fa-facebook"></i>
+          </a>
+        </li>
+        <li>
+          <a href={props.data ? props.data.twitter : "/"}>
+            <i className="fa fa-twitter"></i>
+          </a>
+        </li>
+        <li>
+          <a href={props.data ? props.data.youtube : "/"}>
+            <i className="fa fa-youtube"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
 }
 
 export default Contact;
--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 
 export class Gallery extends Component {
+  state = {
+    jsonContent: {},
+  };
+
+  componentDidMount() {
+    fetch(
+      "https://raw.githubusercontent.com/pkkwilliam/github.io-contents/master/bitcode_website/gallary/gallary.json "
+    );
+  }
+
   render() {
     return (
       <div id="portfolio" className="text-center">
         <div className="container">
           <div className="section-title">
-            <h2>案例</h2>
+            <h2>最近案例</h2>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
               dapibus leonec.
@@ -14,201 +24,52 @@ export class Gallery extends Component {
           </div>
           <div className="row">
             <div className="portfolio-items">
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/01-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Lorem Ipsum</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/01-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/02-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Adipiscing Elit</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/02-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/03-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Lorem Ipsum</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/03-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/04-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Lorem Ipsum</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/04-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/05-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Adipiscing Elit</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/05-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/06-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Dolor Sit</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/06-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/07-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Dolor Sit</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/07-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/08-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Lorem Ipsum</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/08-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/09-large.jpg"
-                      title="Project Title"
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Adipiscing Elit</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/09-small.jpg"
-                        className="img-responsive"
-                        alt="Project Title"
-                      />{" "}
-                    </a>{" "}
-                  </div>
-                </div>
-              </div>
+              <PickTbSection />
             </div>
           </div>
         </div>
       </div>
     );
   }
+}
+
+function PickTbSection() {
+  return (
+    <div>
+      <p>
+        澳提代收管理系統是為澳門龐志空運速递有限公司專門開發的淘寶代收平可快速幫助物流公司建立多功能及健全的快递業務，其中包括手機應
+        用，物流管理，貨物追蹤，難件處理，打印運單...
+        此系統旨在為物流公司及普通商戶創立完善的共赢平台，互惠互利，通過社會化平台分享閒置實物資源或認知盈餘，以低於專業性組織者的邊際成本提供服務並獲得收入的經濟現象。
+      </p>
+      <p>運動系統:</p>
+      <ul>
+        <li>iOS</li>
+        <li>Android</li>
+        <li>Mac</li>
+        <li>Windows</li>
+      </ul>
+      <div className="col-sm-6 col-md-4 col-lg-4">
+        <div className="portfolio-item">
+          <div className="hover-bg">
+            <a
+              href="img/portfolio/01-large.jpg"
+              title="Project Title"
+              data-lightbox-gallery="gallery1"
+            >
+              <div className="hover-text">
+                <h4>Lorem Ipsum</h4>
+              </div>
+              <img
+                src="img/portfolio/01-small.jpg"
+                className="img-responsive"
+                alt="Project Title"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Gallery;
