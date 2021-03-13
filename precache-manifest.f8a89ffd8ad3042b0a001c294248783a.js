b7b0a2f11fe9cddfedf45231354aa2eb16e6f4ea
pkkwilliam <pkkwilliam919@gmail.com>
2021-03-13 12:01:57 +0800

    Updates

---
+++ precache-manifest.f8a89ffd8ad3042b0a001c294248783a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ca6902cd0593ed21c6d3c6c7ad033b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5656ac5000771bd4859e",
+    "url": "/static/css/main.45bf604c.chunk.css"
+  },
+  {
+    "revision": "f7f221fb7d2e002a2f3d",
+    "url": "/static/js/2.e90d392d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e90d392d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5656ac5000771bd4859e",
+    "url": "/static/js/main.0730b3fe.chunk.js"
+  },
+  {
+    "revision": "839a0e94c9436a505b67",
+    "url": "/static/js/runtime-main.3260d7e1.js"
+  }
+]);
