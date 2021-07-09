848ff53f1bb6080f8190f64e4c6744cb5883fb7c
pkkwilliam <pkkwilliam919@gmail.com>
2021-07-09 11:46:39 +0800

    Updates

---
+++ precache-manifest.0073fae055d316524c2ce091a4aab288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b31e97724cd3240b36d2b7f81037bde",
+    "url": "./index.html"
+  },
+  {
+    "revision": "548842de5cb99305c26b",
+    "url": "./static/css/main.45bf604c.chunk.css"
+  },
+  {
+    "revision": "9fb03554b602e1925a6f",
+    "url": "./static/js/2.e2cba26d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e2cba26d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "548842de5cb99305c26b",
+    "url": "./static/js/main.dbc98135.chunk.js"
+  },
+  {
+    "revision": "67215b490597c1261df5",
+    "url": "./static/js/runtime-main.9c062807.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bitcode-website/precache-manifest.156aa9104b3cd4e23931073f29a0c13b.js"
+  "./precache-manifest.0073fae055d316524c2ce091a4aab288.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bitcode-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
